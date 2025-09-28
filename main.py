0b7c6f0b402b09f96b88d225b08aa0d89817e3fe
ahmad jouni <149580606+ahmadjouni1@users.noreply.github.com>
2025-09-28 21:41:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from data.db_sqlite import SchoolDBSqlite
 from gui.app import SchoolApp
+import sys
+print("Choose UI: 1) Tkinter  2) PyQt")
+choice = input("Enter 1 or 2: ").strip()
 
+if choice == "1":
+    import tkinter  
+elif choice == "2":
+    import main_qt  
+else:
+    print("Invalid choice")
+    sys.exit(1)
 if __name__ == "__main__":
-    db = SchoolDBSqlite("school.db")  # creates file if missing
+    db = SchoolDBSqlite("school.db")  
     app = SchoolApp(db)
     app.mainloop()
